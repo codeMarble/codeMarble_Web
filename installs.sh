135ef0e5be423d7ca463f07b5e3c4baf4c18619e
algorithm <dollking@naver.com>
2017-10-11 12:57:44 +0900

    add property parser file

--- installs.sh
+++ installs.sh
@@ -11,30 +11,35 @@ yum_makecache() {
 }
 
 if [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
-  yum clean all
-  yum -y install epel-release
-  yum_makecache
-  yum -y install gcc gcc-c++ kernel-devel
-  yum_makecache
-  yum -y install python-devel
-  yum_makecache
+  if [ "$(id -u)" != "0" ]; then
+    yum clean all
+    yum -y install epel-release
+    yum_makecache
+    yum -y install gcc gcc-c++ kernel-devel
+    yum_makecache
+    yum -y install python-devel
+    yum_makecache
 
-  if [ -z "$(yum list installed | grep mariadb)" ]; then
-    echo 'Install mariaDB'
-    yum -y install mariadb-server
-  fi
+    if [ -z "$(yum list installed | grep mariadb)" ]; then
+      echo 'Install mariaDB'
+      yum -y install mariadb-server
+    fi
 
-  if [ -z "$(which easy_install)" ]; then
-    echo 'Install easy_install'
-    yum -y install python-setuptools
-  fi
+    if [ -z "$(which easy_install)" ]; then
+      echo 'Install easy_install'
+      yum -y install python-setuptools
+    fi
 
-  if [ -z "$(which pip)" ]; then
-    echo 'Install pip'
-    easy_install pip
-  fi
+    if [ -z "$(which pip)" ]; then
+      echo 'Install pip'
+      easy_install pip
+    fi
 
   pip install -r requirements.txt
+
+  else
+    echo 'Run shell script as root.'
+  fi
 
 else
   echo 'Sorry. codeMarble is only available in centos'
